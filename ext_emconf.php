8e062e74e3aa3bd3cbd299e649da565ab3631142
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-11 20:55:34 +0000

    Initial Development
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/feuser_logout/trunk@76453 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "feuser_logout".
+ *
+ * Auto generated 11-06-2013 22:47
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Frontend-user logout',
+	'description' => 'Create content to logout a frontent-user and redirect to loginpage',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Juergen Furrer',
+	'author_email' => 'juergen.furrer@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"9acc";s:12:"ext_icon.gif";s:4:"a9e4";s:17:"ext_localconf.php";s:4:"db82";s:14:"ext_tables.php";s:4:"a45c";s:14:"ext_tables.sql";s:4:"d41d";s:28:"ext_typoscript_constants.txt";s:4:"a6b7";s:24:"ext_typoscript_setup.txt";s:4:"574f";s:13:"locallang.xml";s:4:"d373";s:16:"locallang_db.xml";s:4:"9c82";s:10:"README.txt";s:4:"ee2d";s:14:"doc/manual.sxw";s:4:"5a7b";s:14:"pi1/ce_wiz.gif";s:4:"1604";s:33:"pi1/class.tx_feuserlogout_pi1.php";s:4:"9acb";s:41:"pi1/class.tx_feuserlogout_pi1_wizicon.php";s:4:"efc5";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"8498";}',
+);
+
+?>
---
+++ pi1/class.tx_feuserlogout_pi1.php
@@ -0,0 +1,75 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Juergen Furrer <juergen.furrer@gmail.com>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+
+/**
+ * Plugin 'Frontend-user logout' for the 'feuser_logout' extension.
+ *
+ * @author	Juergen Furrer <juergen.furrer@gmail.com>
+ * @package	TYPO3
+ * @subpackage	tx_feuserlogout
+ */
+class tx_feuserlogout_pi1 extends tslib_pibase
+{
+	public $prefixId      = 'tx_feuserlogout_pi1';
+	public $scriptRelPath = 'pi1/class.tx_feuserlogout_pi1.php';
+	public $extKey        = 'feuser_logout';
+	public $pi_checkCHash = true;
+
+	/**
+	 * The main method of the PlugIn
+	 *
+	 * @param	string		$content: The PlugIn content
+	 * @param	array		$conf: The PlugIn configuration
+	 * @return	The content that is displayed on the website
+	 */
+	public function main($content, $conf)
+	{
+		$this->conf = $conf;
+		$this->pi_setPiVarDefaults();
+		$this->pi_loadLL();
+		// Logoff the user
+		$GLOBALS['TSFE']->fe_user->logoff();
+		$GLOBALS['TSFE']->loginUser = 0;
+		// create the link for redirect
+		$logoutPid = $this->cObj->stdWrap($this->conf['logoutPid'], $this->conf['logoutPid.']);
+		$link = $this->pi_getPageLink($logoutPid, '', array());
+		$url = t3lib_div::locationHeaderUrl($link);
+		// header for redirect
+		t3lib_utility_Http::redirect($url);
+		exit();
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/feuser_logout/pi1/class.tx_feuserlogout_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/feuser_logout/pi1/class.tx_feuserlogout_pi1.php']);
+}
+?>
